f74b91027b93501b3a7f5420347dc1a0e6ce4e2d
Andrea Vieira <aviei008@fiu.edu>
2018-02-04 09:51:17 -0500

    Restaurants Display

---
+++ website/data/restaurants.js
@@ -0,0 +1,16 @@
+let restaurants = [
+	{
+		name: 'Miller's Ale House - Miami Doral',
+		place: '3271 NW 87th Ave, Doral, FL 33172',
+		happyHour: '4:00 - 5:00 PM',
+		menu: 'https://millersalehouse.com/menu/'
+	},
+	{
+		name: 'Flanigan's Seafood Bar and Grill',
+		place: '9857 SW 40th St, Miami, FL 33165'
+		happyHour: ''
+		menu: 'http://www.flanigans.net/menu.php'
+	}
+]
+
+module.exports = restaurants;
